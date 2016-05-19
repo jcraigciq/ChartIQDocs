942e86eae5057c077a236ca22d284b6662c5356c
Justin Craig <jcraig@chartiq.com>
2016-05-19 11:00:32 -0400

    Anchor Scrolling..

--- app/app.js
+++ app/app.js
@@ -1,14 +1,47 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('ciqDocs', [
-  'ngRoute',
-  'ciqDocs.home',
-  'ciqDocs.webTraderGuide',
-  'ciqDocs.upgradeWebTrader',
-  'ciqDocs.mobileQuickstart',
-  'ciqDocs.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/home'});
+var ciqDocs = angular.module('ciqDocs', [
+  'ngRoute', 'ngSanitize', 'ngCsv'
+])
+.config(['$routeProvider', function($routeProvider) {
+$routeProvider.
+	when('/home', {
+    templateUrl: 'pages/home/home.html'}).
+
+	when('/webTraderGuide', {
+    templateUrl: 'pages/webTraderGuide/webTraderGuide.html'}).
+
+	when('/mobileQuickstart', {
+    templateUrl: 'pages/mobileQuickstart/mobileQuickstart.html'}).
+
+	when('/upgradeWebTrader', {
+    templateUrl: 'pages/upgradeWebTrader/upgradeWebTrader.html'}).
+
+   otherwise({redirectTo: '/home'});
 }]);
+
+    ciqDocs.controller('myctrl', function ($scope) {
+      $scope.filename = "test";
+      $scope.persons = [{name: "Justin", age: 27}];
+  // $scope.persons = new array();
+  //$scope.persons = {};
+
+      $scope.addData = function(name, age) {
+        $scope.persons.push({name: name, age: age });
+      };
+
+      $scope.getHeader = function () {return ["name", "age"]};
+    });
+
+ciqDocs.controller('ScrollController', ['$scope', '$location', '$anchorScroll',
+  function ($scope, $location, $anchorScroll) {
+    $scope.hashScroll = function(scrollTo) {
+      // set the location.hash to the id of
+      // the element you wish to scroll to.
+      $location.hash(scrollTo);
+
+      // call $anchorScroll()
+      $anchorScroll();
+    };
+  }]);
